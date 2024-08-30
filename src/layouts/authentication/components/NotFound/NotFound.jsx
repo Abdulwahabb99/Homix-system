972bf2330f01947559893a0180872b6a3661ca18
abdulwahab999 <abdulwahab.shalaby@garment.io>
2024-08-31 02:49:21 +0300

    change not found page

--- src/layouts/authentication/components/NotFound/NotFound.jsx
+++ src/layouts/authentication/components/NotFound/NotFound.jsx
@@ -3,22 +3,23 @@ import Typography from "@mui/material/Typography";
 import ArrowNextIcon from "@mui/icons-material/ArrowForward";
 import { IconButton } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 
 const NotFound = () => {
   const navigate = useNavigate();
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <div style={{ display: "flex", alignItems: "center", marginBottom: "12px" }}>
-        <IconButton color="#344767" onClick={() => navigate("/home")}>
-          <ArrowNextIcon />
-        </IconButton>
-      </div>
+    <DashboardLayout>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <div style={{ display: "flex", alignItems: "center", marginBottom: "12px" }}>
+          <IconButton color="#344767" onClick={() => navigate("/home")}>
+            <ArrowNextIcon />
+          </IconButton>
+        </div>
 
-      <Typography variant="h3" gutterBottom>
-        الصفحة غير موجودة
-      </Typography>
-      <Typography variant="body1">الصفحة التي تبحث عنها غير موجودة.</Typography>
-    </div>
+        <Typography variant="h3">الصفحة غير موجودة</Typography>
+        <Typography variant="body1">الصفحة التي تبحث عنها غير موجودة.</Typography>
+      </div>
+    </DashboardLayout>
   );
 };
 
