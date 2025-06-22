8be3c321c796d947f79ca30dd968a8515e014c6b
ibrahim-nader <ibrahim@garment.io>
2025-06-22 09:47:17 +0000

    fix: Correct variable declaration for existing user lookup in user service

--- app/modules/user/user.service.js
+++ app/modules/user/user.service.js
@@ -274,7 +274,7 @@ class UserService {
           await user.destroy();
         }
       } else {
-        ExistingUser = User.findOne({
+        const ExistingUser = User.findOne({
           where: {
             email: `${vendor.name.toLowerCase()}@${
               process.env.SHOPIFY_STORE
